cf2929e89d062f0622cc40b6a5f20fadbc29687a
Amit Patil <amitpatil@Amits-MacBook-Air.local>
2022-12-20 20:04:20 -0600

    This is my first commit

--- main.py
+++ main.py
@@ -4,14 +4,16 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def displayName(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Amit')
+    displayName('Amit')
+    displayName('Vivaan')
+    displayName('Riaan')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
