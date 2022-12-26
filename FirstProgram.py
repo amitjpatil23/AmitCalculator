d6b8dfe317af27e7ef391d2ce3d5c94bb6575884
Amit Patil <amitpatil@Amits-MacBook-Air.local>
2022-12-26 13:45:31 -0600

    check point testing some python

---
+++ FirstProgram.py
@@ -0,0 +1,67 @@
+# first_name = ("Tony")
+# last_name = ("stark")
+# age = ("51")
+# is_a_genious = ("True")
+# name = "aman"
+# age = 24
+# print(first_name + " " + last_name)
+# print(age)
+# print(is_a_genious)
+# superhero = input('what is your superhero name? ')
+# print(superhero)
+
+# first = input("enter first number : ")
+# second = input("enter second number")
+#
+# sum = int(first) + int(second)
+# print("the sum is : " +str(sum))
+#
+# # name = "Tony"
+# # print(name.replace("Tony" , "ironman"))
+
+
+
+# name= "amit patil"
+# print(name.find("p"))
+# print(name.replace("amit patil","sumit patil"))
+
+# #arithmatic oprator
+# i = 5  #5
+# print (i)
+# i = i+2 #7
+# print (i)
+# i += 2 #9
+# print (i)
+# i -= 2 #7
+# print (i)
+# i *= 2 #14
+# print (i)
+
+#
+# result = 3 + 5 * 2 # 16 or 13 ?
+# print(result)
+#
+# #anything divide by 0 is invalid
+# #anything power to 0 is 1
+#
+#
+# print(3 != 2)
+#
+#
+# print(2 > 3 or 2 > 1)
+
+# age = 17
+#
+# if age >= 18 or (age >=15 and age<=20):
+#     print("you can ride a bike")
+#     print("You can apply for licance")
+# # elif age >=15 and age<=20:
+# #     print("You can apply for licance")
+# elif age < 20:
+#     print("you cannot ride a bike")
+# else:
+#     print("thank you")
+#
+# print("thank you")
+
+
--- main.py
+++ main.py
@@ -4,17 +4,20 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def displayName(name):
+def displayName(boc):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, {boc}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    displayName('Amit')
-    displayName('Vivaan')
-    displayName('Riaan')
-    displayName('Gayatri')
+    displayName('Amit 35')
+    displayName('Vivaan 8')
+    displayName('Riaan 3')
+    displayName('Gayatri 32')
+    displayName('mom 56')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
+print("hello coders")
+print('a_b')
+print("1.2")
+print("apna college")
